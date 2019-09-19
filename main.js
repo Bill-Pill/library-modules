5235e771030e852b8a9d7dc51d4b27213eb74dcd
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-09-18 23:06:08 -0400

    Initial Commit

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
-// write your code here to make the tests pass
+var Library = function(){
+  var books = [];
+
+  var addBook = function(book) {
+    books.push(book)
+  }
+
+  var checkOutBook = function(book) {
+    
+  }
+
+  return { 
+    addBook:addBook,
+    checkOutBook:checkOutBook
+             }
+}
+
+var Book = function(title, author){
+  var attributes = {
+    title : title,
+    author : author
+  };
+  var getAttribute = function(attribute) {
+    if (attributes[attribute]) {
+      return attributes[attribute];
+    }
+    else return false;
+  }
+   return ({getAttribute : getAttribute})
+}
